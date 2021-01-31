e26b57c42ab406d2a38623279cace9c9b099dae1
Pooja <kpooja.193@gmail.com>
2021-01-31 00:56:08 -0600

    Geocoding

--- app.py
+++ app.py
@@ -1,43 +1,72 @@
-from flask import Flask, render_template, request
-from flask_sqlalchemy import SQLAlchemy
+from flask import Flask, render_template, request, jsonify
+import firebase_admin
+from firebase_admin import credentials, firestore
+import requests
+from flask_googlemaps import GoogleMaps, Map
+
+
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
-db = SQLAlchemy(app)
 
-class Drink(db.Model):
-    id = db.Column(db.Integer, primary_key = True)
-    name = db.Column(db.String(80), unique = True, nullable = False)
-    description = db.Column(db.String(120))
+cred = credentials.Certificate("animals-a662f-firebase-adminsdk-29wae-4cd84b0745.json")
+firebase_admin.initialize_app(cred)
+db = firestore.client()
+animals_data = db.collection(u'animals')
 
-    def __repr__(self):
-        return f"{self.name} - {self.description}"
+# @app.route('/')
+# def map_func():
+# 	return render_template('map.html',apikey=api_key,latitude=latitude,longitude=longitude)#map.html is my HTML file name
+
+# @app.route('/drinks')
+# def get_drinks():    
+#     drinks = [drink.to_dict() for drink in drink_data.get()]
+#     return jsonify(drinks), 200
+
+# @app.route('/drinks/<name>')
+# def get_drink(name):
+#     drinks = [drink.to_dict() for drink in drink_data.get()]
+#     for drink in drinks:
+#         if drink['name'] == name:
+#             return drink
+#     return "Drink not found"   
+
+# @app.route('/drinks', methods=['POST'])
+# def add_drink():
+#     drink_data.add({'name': request.json['name'], 'description': request.json['description']})
+#     return {"message":"added drink"}
+
+# @app.route('/drinks/delete/<name>')
+# def del_drink(name):
+#     drink = request.args.get(name)
+#     drink_data.document(drink).delete()
+#     return {"message" : "drink deleted"}
+
+# api_key = "AIzaSyCcHTOdNeHwZ91F_C8e9ToQoVnpXn_D9Ag" # change this to your api key
+# # get api key from Google API Console (https://console.cloud.google.com/apis/)
+# GoogleMaps(app, key=api_key) # set api_key
 
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return 'Bad drinks flask app'
+    if request.method == "POST":
+        name = request.form.get('name')
+        lat = request.form.get('lat')
+        lng = request.form.get('lng')
+        animals_data.document(str(len(list(animals_data.get())) + 1)).set(({'name': name, 'lat': lat, 'lng':lng}))
+    
+    animals = []
+    animals = [x.to_dict() for x in list(animals_data.get())]
+    return render_template('geolocation.html', animals=animals) # render template
 
-@app.route('/drinks')
-def get_drinks():
-    drinks = Drink.query.all()
-    output = []
-    for drink in drinks:
-        drink_data = {'name':drink.name, 'description':drink.description}
-        output.append(drink_data)
-    return {'drinks':output}
+# def add_drink():
+#     drink_data.add({'name': request.json['name'], 'description': request.json['description']})
 
-@app.route('/drinks/<id>')
-def get_drink(id):
-    drink = Drink.query.get_or_404(id)
-    return {'name':drink.name, 'description':drink.description}
-
-@app.route('/drinks', methods=['POST'])
-def add_drink():
-    drink = Drink(name=request.json['name'], description=request.json['description'])
-    db.session.add(drink)
-    db.session.commit()
-    return {'id':drink.id}
+@app.route('/getdata', methods=['GET', 'POST'])
+def getdata():
+    json_data = requests.get.args('json')
+    return json_data
+    # you can use this to get request with strings and parse json
+    # put data in database or something
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='127.0.0.1', port=5000, debug=True)
---
+++ geolocation.js
@@ -0,0 +1,38 @@
+let G, options, spans;
+
+document.addEventListener('DOMContentLoaded', init);
+
+function init(){
+    if(navigator.geolocation){
+        let giveUp = 1000 * 30;  //30 seconds
+        let tooOld = 1000 * 60 * 60;  //one hour
+        options ={
+            enableHighAccuracy: true,
+            timeout: giveUp,
+            maximumAge: tooOld
+        }
+        
+        navigator.geolocation.getCurrentPosition(gotPos, posFail, options);
+    }else{
+        //using an old browser that doesn't support geolocation
+    }
+}
+
+function gotPos(position){
+    spans = document.querySelectorAll('p span');
+    spans[0].textContent = position.coords.latitude;
+    spans[1].textContent = position.coords.longitude;
+    spans[2].textContent = position.coords.accuracy;
+    
+    spans[6].textContent = position.timestamp;
+}
+
+function posFail(err){
+    //err is a number
+    let errors = {
+        1: 'No permission',
+        2: 'Unable to determine',
+        3: 'Took too long'
+    }
+    document.querySelector('h1').textContent = errors[err];
+}
